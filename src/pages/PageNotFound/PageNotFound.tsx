6f1cc383af13a006259a841ea1033e76fb4a7b7a
JeanRou02 <jeannicolasrouette@gmail.com>
2025-03-09 15:25:04 -0600

    Updated PageNot Found With Current Components

--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -1,14 +1,21 @@
 
 // PageNotFound.tsx
 
-import { Link } from "react-router-dom";
+import {  useNavigate } from "react-router-dom";
 import logo from "../../assets/waste-bins-medium.png"
 import FlexBackGround from "@/Components/FlexBackGround";
 import Block from "@/Components/Block";
 import FlexRow from "@/Components/FlexRow";
+import Button from "@/Components/Button";
 
 
 function PageNotFound() {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate(`/`);
+  };
+
   return (
     <FlexBackGround>
       <FlexRow>
@@ -16,7 +23,13 @@ function PageNotFound() {
           <img alt={'bins'} src={logo}></img>
           <h1>Uh-oh</h1>
           <p>404 Page not found</p>
-          <Link to="/">Go back home</Link>
+          <Button
+            dark={true}
+            onClick={handleButtonClick}
+            containerStyle={{ marginTop: "15px" }}
+          >
+            Go Back Home
+          </Button>
         </Block>
       </FlexRow>
     </FlexBackGround>
