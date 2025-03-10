511bfee88e99fc1c1ebc0ef2289b5c8602b34b40
JeanRou02 <jeannicolasrouette@gmail.com>
2025-03-09 19:26:24 -0600

    Began Layout for ChatBot

--- src/pages/General/General.tsx
+++ src/pages/General/General.tsx
@@ -4,14 +4,42 @@
 import FlexBackGround from "@/Components/FlexBackGround";
 import Block from "@/Components/Block";
 import FlexRow from "@/Components/FlexRow";
+import TextInput from "@/Components/TextInput";
+import { useState } from "react";
+import Button from "@/Components/Button";
 
 
 function General() {
+
+  const [chatQuestion, setChatQuestion] = useState("")
+
+  const handleButtonClick = () => {
+    console.log("Button Clicked!")
+  };
+
+
   return (
     <FlexBackGround>
       <FlexRow>
-        <Block>
-          General
+        <Block style={{height: "100%"}}>
+          <FlexRow>
+            Chat Goes Here
+          </FlexRow>
+          <FlexRow style={{flex: "none"}}>
+            <TextInput
+              value={chatQuestion}
+              onChange={setChatQuestion}
+              placeholder="E.g. Where do I throw out batteries?"
+              containerStyle={{ maxWidth: "50%", minWidth: "100px" }}
+            />
+            <Button
+              dark={true}
+              onClick={handleButtonClick}
+              containerStyle={{ maxWidth: "10%", minWidth: "50px" }}
+            >
+              Ask
+            </Button>
+          </FlexRow>
         </Block>
       </FlexRow>
     </FlexBackGround>
