93dbd798ca618f9cb0b04ccdcf0e4d8b4566964c
Talha Siddiqui <talhasiddiqui922000@gmail.com>
2025-03-11 22:23:28 -0600

    fix: base path issue for production build

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-
-// App.tsx
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./router/AppRouter";
+
+const basename = import.meta.env.MODE === "production" ? "/what-goes-where" : "/";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <AppRouter />
-        </BrowserRouter>
+      <BrowserRouter basename={basename}>  {/* 👈 Pass basename here */}
+        <AppRouter />
+      </BrowserRouter>
     </QueryClientProvider>
   );
 }
