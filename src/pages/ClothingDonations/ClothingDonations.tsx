0bbe56740da389a4ec8163ea8e7f32e015349a11
JeanRou02 <jeannicolasrouette@gmail.com>
2025-04-11 00:02:20 -0600

    fix: update ClothingDonations layout and add navigation button for old clothes disposal

--- src/pages/ClothingDonations/ClothingDonations.tsx
+++ src/pages/ClothingDonations/ClothingDonations.tsx
@@ -6,11 +6,14 @@ import styles from "./ClothingDonations.module.css";
 import { CLOTHING_DONATION_CENTERS } from "./constants";
 import FlexBackGround from "@/Components/FlexBackGround";
 import Block from "@/Components/Block";
+import Button from "@/Components/Button";
+import { useNavigate } from "react-router-dom";
 
 function ClothingDonations() {
   const [selectedLocation, setSelectedLocation] = useState<Location | null>(
     null
   );
+  const navigate = useNavigate();
   return (
     <FlexBackGround style={{ justifyContent: "flex-start", overflowY: "auto" }}>
       <FlexRow style={{ flex: "none" }}>
@@ -20,9 +23,18 @@ function ClothingDonations() {
             <p>
               Fast fashion has led to massive textile waste and pollution, but
               small actions like donating clothes and shopping second-hand can
-              make a big difference.
+              make a big difference.<br/> Learn more about disposing of old clothes by clicking the button below.
             </p>
           </div>
+          <FlexRow style={{ flex: "none", padding: "0px", justifyContent: "flex-start" }}>
+            <Button
+              onClick={() => navigate("/item/oldclothes")}
+              dark={false}
+              containerStyle={{maxWidth: "40%"}}
+            >
+              Copper Wires
+            </Button>
+          </FlexRow>
         </Block>
       </FlexRow>
       <FlexRow>
